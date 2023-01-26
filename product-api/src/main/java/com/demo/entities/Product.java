d735125ebe27e2a7743f557925c497d3e996fe80
Jagan Singh <jagansiiinghchoudhary@gmail.com>
2023-01-26 23:48:37 +0000

    k8s

--- product-api/src/main/java/com/demo/entities/Product.java
+++ product-api/src/main/java/com/demo/entities/Product.java
@@ -1,6 +1,7 @@
 package com.demo.entities;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -8,15 +9,19 @@ import java.math.BigDecimal;
 /**
  * Product entity.
  */
+@Getter
+@Setter
 @Entity
-@Data
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(name = "productId")
     private String productId;
+    @Column(name = "name")
     private String name;
+    @Column(name = "price")
     private BigDecimal price;
 
 }
