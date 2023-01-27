d73ebb95f8ea8af06b3b1a118068d98ec5c07b2c
Jagan Singh <jagansiiinghchoudhary@gmail.com>
2023-01-27 00:32:13 +0000

    k8s

--- product-api/README.md
+++ product-api/README.md
@@ -2,8 +2,8 @@
 
 Spring Boot CRUD Application
 
-### Process
-- kubectl apply -f mysql-secret.yaml
-- kubectl apply -f mysql-deployment.yaml
-- kubectl apply -f product-deployment.yaml
-- minikube service product --url
+### Process k8s folder
+- 1
+- 2
+- 3
+- 4
--- product-api/src/main/java/com/demo/entities/Product.java
+++ product-api/src/main/java/com/demo/entities/Product.java
@@ -2,7 +2,10 @@ package com.demo.entities;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
