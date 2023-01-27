ff9391a38560b064ae4132682e861c4f13f9953f
Jagan Singh <jagansiiinghchoudhary@gmail.com>
2023-01-27 15:48:40 +0000

    frontend

--- my-shopping-app/src/App.js
+++ my-shopping-app/src/App.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+const url = "http://product-api.info/products";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: []
-    }
+      product: [],
+    };
     this.delete = this.delete.bind(this);
+    this.create = this.create.bind(this);
   }
- delete(post) {
- console.log('Delete start')
-     const url = "http://product-api.info/products"+"/"+post.id;
-      fetch(url, { method: 'DELETE' })
-             .then(() => console.log('Delete successful'));
+  create(event) {
+    alert("Refresh");
+    event.preventDefault();
+    const text = {
+      name: "A Product",
+      price: 10000,
+      productId: "test123",
+    };
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(text),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => console.log("Delete successful"));
+  }
+
+  delete(post) {
+    console.log("Delete start");
+    fetch(url+'/' + post.id, { method: "DELETE" }).then(() =>
+      console.log("Delete successful")
+    );
   }
   componentDidMount() {
-    const url = "http://product-api.info/products";
     fetch(url)
-    .then(response => response.json())
-    .then(json => this.setState({ product: json }))
+      .then((response) => response.json())
+      .then((json) => this.setState({ product: json }));
   }
 
   render() {
@@ -29,24 +48,38 @@ class App extends Component {
           <h1 class="display-4">Product List</h1>
         </div>
         <div class="row">
-        {product.map((post) => (
-          <div class="col-sm-6">
-          <div className="card" key={post.id}>
-            <div className="card-header">
-              # {post.id}
-            </div>
-            <div className="card-body">
-              <p className="card-text">{post.name}</p>
-              <button onClick={this.delete(post)} class="btn btn-primary">Delete</button>
-            </div>
-             <div class="card-footer text-muted">
-                {post.price}€
+          {product.map((post) => (
+            <div class="col-sm-6">
+              <div className="card text-white bg-dark mb-3" key={post.id}>
+                <div className="card-header"># {post.id}</div>
+                <div className="card-body">
+                  <p className="card-text">{post.name}</p>
+                  <button
+                    onClick={() => this.delete(post)}
+                    class="btn btn-primary"
+                  >
+                    Delete
+                  </button>
+                </div>
+                <div class="card-footer text-muted">{post.price}€</div>
               </div>
             </div>
-            </div>
-        ))}
-       </div>
+          ))}
 
+          <div class="col-sm-6">
+            <div className="card text-white bg-dark mb-3">
+              <div className="card-header"># Create New Product</div>
+              <div className="card-body">
+                <form onSubmit={this.create}>
+                  <button type="submit" class="btn btn-primary">
+                    Create Sample Product
+                  </button>
+                </form>
+              </div>
+              <div class="card-footer text-muted"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
